ffec956abd73165ea78dfda5bd9ed6bd252417d5
naske <naske777ig@gmail.com>
2024-07-11 23:21:00 -0400

    feat: Add data for links navegation

---
+++ src/app/data.js
@@ -0,0 +1,18 @@
+export const navigationLinks = [
+  { href: "/#Servicios", label: "Servicios" },
+  { href: "/#Pasos", label: "Pasos" },
+  { href: "/#Equipo", label: "Nuestro Equipo" },
+  { href: "/#Trabajos", label: "Trabajos" },
+  { href: "/#Comentarios", label: "Comentarios" },
+  { href: "/#Contacto", label: "Contacto" },
+  { href: "/blog", label: "Blog" },
+];
+export const knowMore = { href: "#Mas", label: "Conocer Más" };
+export const navegationLinks2 = [
+  knowMore,
+  {
+    href: "#Contactar",
+    label: "Contactar",
+    extraClasses: "text-white bg-[#0AC05E] rounded-3xl p-4",
+  },
+];
